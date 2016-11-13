d485574fd945898b07536ee50131c08e9fc92389
kevin <addinkevin@gmail.com>
2016-11-13 14:44:06 -0300

    Paso el codigo del main a un main propio del viajante.

--- main.cpp
+++ main.cpp
@@ -2,48 +2,16 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include "Viajante/ParserTSPFile.h"
-#include "Viajante/Types.h"
-#include "Viajante/TSPTest.h"
+#include "Viajante/MainViajante.h"
 
 using namespace std;
 
-void printMatrix(vector<vector<CostInt>>* matrix) {
-    for (vector<vector<CostInt>>::iterator it = matrix->begin(); it != matrix->end(); ++it) {
-        vector<CostInt> items = *it;
-        for (vector<CostInt>::iterator it2 = items.begin(); it2 != items.end(); ++it2) {
-            CostInt x = *it2;
-            std::cout << int(x) << "\t";
-        }
-        std::cout << std::endl;
-    }
-}
-
-void printSolution(vector<VertexInt>* solutionList) {
-    for (vector<VertexInt>::iterator it = solutionList->begin(); it != solutionList->end(); ++it) {
-        VertexInt x = *it;
-        std::cout << int(x) << std::endl;
-    }
-    std::cout << std::endl;
-}
 
 int main() {
     try{
         std::cout << "Hello, World!" << std::endl;
 
-        /*
-        ParserTSPFile parserTSPFile("./Viajante/files/tsp2.txt", "./Viajante/files/tsp2_s.txt");
-        vector<vector<CostInt>>* matrix = parserTSPFile.getMatrix();
-        //printMatrix(matrix);
-
-        vector<VertexInt>* solutionList = parserTSPFile.getSolutionList();
-
-        printSolution(solutionList);
-         */
-
-        TSPTest test;
-        //test.runExample("./Viajante/files/prueba.txt", "./Viajante/files/prueba_s.txt");
-        test.runExample("./Viajante/files/tsp2.txt", "./Viajante/files/tsp2_s.txt");
+        //MainViajante().run();
 
     } catch(string error) {
         std::cout << error;

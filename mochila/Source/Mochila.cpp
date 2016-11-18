abed15033073f3d60edbcff6f7cddf49dc04b4a5
gatti2602 <gatti2602@gmail.com>
2016-11-17 21:29:12 -0300

    Imprime objetos que quedan en la mochila y forman parte de la solucion optima

--- mochila/Source/Mochila.cpp
+++ mochila/Source/Mochila.cpp
@@ -9,6 +9,7 @@
 #include "../Headers/Mochila.h"
 #include <iostream>
 #include <list>
+#include <set>
 #include "../Headers/Parser.h"
 #include "../Headers/Stage.h"
 #include "../Headers/Node.h"
@@ -32,33 +33,57 @@ int Mochila::initMochila() {
 }
 
 void Mochila::computarMochila(Stage stage){
-	cout << "ComputarMochila: start" << endl;
+	//cout << "ComputarMochila: start" << endl;
+	int tPesos[stage.qtyNode+1][stage.capacityBag+1];
+	std::set<int> tSoluciones[stage.qtyNode+1][stage.capacityBag+1];
+	std::set<int> empty;
 	try{
-		int tabla[stage.qtyNode+1][stage.capacityBag+1];
-		for(int i=0;i<=stage.capacityBag;i++)
-			tabla[0][i] = 0;
+
+		for (int i=0;i<stage.qtyNode;i++)
+			for(int j=0;j<=stage.capacityBag;j++){
+				tPesos[i][j] = 0;
+				tSoluciones[i][j] = empty;
+
+			}
 
 		for(int i=1; i<=stage.qtyNode; i++){
 			for(int w=0;w<=stage.capacityBag;w++){
 				int weight = stage.getNode(i-1)->weight;
 				int valor = stage.getNode(i-1)->valor;
 				if(w < weight){
-					tabla[i][w] = tabla[i-1][w];
+					tPesos[i][w] = tPesos[i-1][w];
+					tSoluciones[i][w] = tSoluciones[i-1][w];
 				}
 				else{
-					if(tabla[i-1][w] > (valor + tabla[i-1][w-weight]))
-						tabla[i][w] = tabla[i-1][w];
-					else
-						tabla[i][w] = valor + tabla[i-1][w-weight];
+					if(tPesos[i-1][w] > (valor + tPesos[i-1][w-weight])){
+						tPesos[i][w] = tPesos[i-1][w];
+						tSoluciones[i][w] = tSoluciones[i-1][w];
+					}
+					else{
+						tPesos[i][w] = valor + tPesos[i-1][w-weight];
+						tSoluciones[i][w] = tSoluciones[i-1][w-weight];
+						tSoluciones[i][w].insert(i);
+					}
 				}
 				//cout<< "w"<<w<<endl;
 			}
 			//cout << "i" << i<< endl;
 		}
-		cout << "ComputarMochila: RESULTADO: " << tabla[stage.qtyNode][stage.capacityBag] << endl;
+		cout << "ComputarMochila: RESULTADO: " << tPesos[stage.qtyNode][stage.capacityBag] << endl;
 	} catch(string & error) {
 		std::cout << "***ERROR***: "<< error << endl;
 	}
+
+	//Imprimo objetos en la mochila
+
+	std::set<int> mochilaFinal;
+	mochilaFinal.swap(tSoluciones[stage.qtyNode][stage.capacityBag]);
+
+
+	for ( std::set<int>::const_iterator si = mochilaFinal.begin( ) ;
+		 si != mochilaFinal.end( ) ; si++ ) {
+	      std::cout << *si << "\n" ;
+	   }
 }
 Mochila::~Mochila(){
 

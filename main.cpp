4fe8eb1500bf129a6ee8092eacad251a0abf9aa7
gatti2602 <gatti2602@gmail.com>
2016-11-18 19:31:03 -0300

    string con rutas

--- main.cpp
+++ main.cpp
@@ -18,9 +18,20 @@ int main() {
         //MainViajante().run();
         //MainFlujoDeRedes().run();
 
-        // Mochila test
-        string ruta = "./mochila/files/knapPI_16_100_1000.csv";
-        Mochila* mochila = new Mochila(ruta);
+        //Mochila test easy instances
+        string rutaEasy1= "./mochila/files/knapPI_1_50_1000.csv";
+        string rutaEasy2= "./mochila/files/knapPI_2_50_1000.csv";
+        string rutaEasy3= "./mochila/files/knapPI_3_50_1000.csv";
+        string rutaEasy4= "./mochila/files/knapPI_4_50_1000.csv";
+        string rutaEasy5= "./mochila/files/knapPI_5_50_1000.csv";
+        string rutaEasy6= "./mochila/files/knapPI_6_50_1000.csv";
+        string rutaEasy7= "./mochila/files/knapPI_9_50_1000.csv";
+
+        // Mochila test hard instances
+        string rutaHard1= "./mochila/files/knapPI_16_100_1000.csv";
+        string rutaHard2 = "./mochila/files/knapPI_16_500_1000.csv";
+        string rutaHard3 = "./mochila/files/knapPI_16_1000_1000.csv";
+        Mochila* mochila = new Mochila(rutaHard1);
 
     } catch(string & error) {
         std::cout << "***ERROR***: "<< error << endl;
